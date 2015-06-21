892a6f28f72d602ec08ea455cbe8d201165b7513
rehmmarkusgit config --global user.email rehmma@gmail.com <rehmma@gmail.com>
2015-06-21 21:55:57 +0200

    final v2

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,11 @@
 # You should create one R script called run_analysis.R that does the following. 
-# Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names. 
-# From the data set in step 4, creates a second,
+# 1) Merges the training and the test sets to create one data set.
+# 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3) Uses descriptive activity names to name the activities in the data set
+# 4) Appropriately labels the data set with descriptive variable names. 
+# 5) From the data set in step 4, creates a second,
 #   independent tidy data set with the average of each variable for each activity and each subject.
+
 
 getwd()
 setwd("/Users/markushome/Desktop/UCI_HAR_Dataset")
@@ -82,7 +83,11 @@ library(dplyr)
 data <- data[ ,2:82]
 
 # create tidy data set- group by participant (former "subject") and activity
-tidy <- data %>% group_by(participant, activity.name) %>% summarise_each(funs(mean))
+tidy_temp <- data %>% group_by(participant, activity.name) %>% summarise_each(funs(mean))
+
+# melt data to long format
+library(reshape2)
+tidy <- melt(tidy_temp, id = c("participant","activity.name"))
 
 # export to txt file
 write.table(tidy, file="tidy.txt", row.name=FALSE) 
